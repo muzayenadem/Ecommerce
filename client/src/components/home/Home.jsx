593e92ba94c0cb5d850444d913d287e26b1596f8
muzayenadem <muzynadem@gmail.com>
2024-02-08 00:42:45 +0300

    something

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
+import log from  './kurs.jpg'
 function Home() {
   return (
-    <div className='w-full h-auto flex flex-wrap m-7 p-5 '>
-      <div className='w-3/4 ml-32 text-center bg-indigo-600 rounded-xl shadow-2xl flex flex-wrap  justify-between '>
+    <section className='gird w-full h-auto flex flex-wrap m-7 p-1 '>
+      <div className=' w-screen md:items-center md:text-left md:mr-10 text-center bg-indigo-600 rounded-xl shadow-2xl flex flex-wrap  justify-between '>
         <div className='w-3/4'>
         <input
         type='search'
         placeholder='search here'
-        className='rounded-2xl px-3 py-4 shadow-xl bg-white text-left border-white m-4 w-3/4 '
+        className='rounded-2xl px-3 py-4 shadow-xl bg-white text-left border-white m-4 w-3/4 focus:border-none focus:outline-none'
         />
         <button className='bg-violet-950 text-center text-white font-bold px-10 py-3 rounded-2xl '>Search</button>
         </div>
        
         <h1 className='text-left ml-7 text-gray-300 font-light font-serif text-2xl px-3 py-5'>Eccommers Website</h1>
+      </div >
+      <div className='grid mt-5 place-items-center  grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-5 w-full'>
+      {
+        data.map((result)=>{
+          return     <div className='w-[200px] h-[200px] '>
+          <img src={log}  alt="" 
+        className=' block sm:text-cen '
+        />
+       <p className='font-light font-sans text-left '>product name</p>
+       <p className='text-indigo-600'>price $45</p>
+       <p className='text-center'>this is very rated products</p>
+          </div>
+        })
+      }
+    
       </div>
-    </div>
+    </section>
   )
 }
 
 export default Home
+
+const data = [1,2,3,4,5]
