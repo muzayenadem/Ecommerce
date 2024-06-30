09b568d27bd97be9032e2d07ca0c0c2b48c2fb56
muzayenadem <muzynadem@gmail.com>
2024-06-30 21:30:03 +0300

    changing server from local to global

--- client/src/components/drowbars/LoginDrowbar.jsx
+++ client/src/components/drowbars/LoginDrowbar.jsx
@@ -32,9 +32,7 @@ function LoginDrowbar({}) {
        dispatch(noOpend())
       await axios.post('https://ecommerce-8yhy.onrender.com/login',{email,password})
        .then(()=>{
-           window.location = 'http://localhost:5173/home'
-          //navigate('/home')
-          //window.relaod = 'http://localhost:5173/home'
+           window.location = 'https://ecommerce-client-coral.vercel.app/home'
        })
        .catch((err)=>{
           console.log(err.message)
--- client/src/components/drowbars/ProfileDrawbar.jsx
+++ client/src/components/drowbars/ProfileDrawbar.jsx
@@ -32,7 +32,7 @@ function ProfileDrawbar({opened,nopend}) {
     const distpatch = useDispatch()  
     const logoutHandler = () =>{
       axios.get('https://ecommerce-8yhy.onrender.com/logout')
-     window.location='http://localhost:5173'
+     window.location='https://ecommerce-client-coral.vercel.app'
  
     }
     useEffect(()=>{
