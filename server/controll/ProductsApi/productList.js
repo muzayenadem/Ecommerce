25b228557d7a717833b68482109dded1217ff0ec
muzayenadem <muzynadem@gmail.com>
2024-06-30 19:55:26 +0300

    changing server from local to global

--- server/controll/ProductsApi/productList.js
+++ server/controll/ProductsApi/productList.js
@@ -4,6 +4,7 @@ const productList = async(req,res)=>{
     try {
         const product = await productModel.find({})
         res.json(product)
+        console.log(product)
     } catch (error) {
         res.status(500).json({err:"some error"})
     }
