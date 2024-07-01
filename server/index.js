52a680e059f6f243508562b1a1f1dc10df7bc5d5
muzayenadem <muzynadem@gmail.com>
2024-07-01 16:07:17 +0300

    little change on logged in page

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.static('Files'))
 app.use(cors({
-    origin:['https://ezasco.vercel.app','http://localhost:5173'],
+    origin:['https://ezasco.vercel.app','https://ezasco-git-ecommerce-muzayen.vercel.app','http://localhost:5173'],
      credentials:true,
      methods: 'GET,POST,PUT,DELETE',
      allowedHeaders: 'Content-Type,Authorization'
