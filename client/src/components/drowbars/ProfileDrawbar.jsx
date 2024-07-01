0337a9d16f6b18a4a4a090970f38f956511b3170
muzayenadem <muzynadem@gmail.com>
2024-07-01 14:40:19 +0300

    little change on logged in page

--- client/src/components/drowbars/ProfileDrawbar.jsx
+++ client/src/components/drowbars/ProfileDrawbar.jsx
@@ -37,7 +37,7 @@ function ProfileDrawbar({opened,nopend}) {
         console.log(response.data);
         // Remove the token cookie from the client side
         Cookies.remove('user', { path: '/' });
-        navigate('/')
+        window.location= 'https://ezasco.vercel.app'
       } catch (error) {
         console.error('There was a problem with the logout request:', error);
       }
