fc1672bb6e917a3d5e040aa5fe6e23f66bcc0f08
muzayenadem <muzynadem@gmail.com>
2024-07-01 14:18:50 +0300

    little change on logged in page

--- server/router/routes.js
+++ server/router/routes.js
@@ -99,10 +99,14 @@ router.route('/usermessagedata:id').get(auth,userMessageData)
 
 router.route('/logout').get((req,res)=>{
     res.clearCookie('user', { path: '/' });
-    const token = req.cookies.user
+    const userToken = req.cookies.user
     res.status(200).json({ message: 'Logged out successfully' });
     console.log('Logged out successfully')
-    console.log({token})
+    console.log({userToken})
+    req.cookies.user = 'not cookies'
+    const deletedtoken = req.cookies.user
+    console.log({deletedtoken})
+
 })
 router.route('/logoutAdmin').get((req,res)=>{
     res.cookie("adminLoginToken","",{
