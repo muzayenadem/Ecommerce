c6536325bdabc5d3234e7ab5ddc4c84df962e809
muzayenadem <muzynadem@gmail.com>
2024-07-02 22:17:22 +0300

    changing the file storage from multer to firebase

--- server/controll/updateProfile.js
+++ server/controll/updateProfile.js
@@ -4,55 +4,48 @@ const updateProfile = async(req,res)=>{
     try {
 
         const {image,firstName,lastName,email,password,phone,address,gender,userId} = req.body
-
-        // const splitedTags =  tags && tags.split(',')
-        // const splitedCategory = category && category.split(',')\
-        // const imageName = req.file.filename
-        console.log({image:image[0].name,firstName,email,password})
-        const asure = req.file == undefined ? true : false
-       
-        if(!asure){
-         const imageName = req.file.filename
-            const profileUpdate = await usersModel.findOneAndUpdate(
-                {_id:userId},
-                {
-                     image:image,
-                     firstName,
-                     lastName,
-                     email,
-                     password,
-                     phone,
-                     address,
-                     gender,
-                },
-                {new:true}
-            )
-            if(profileUpdate){
-               res.status(200).send('profile is succesfully updated')
-               } else
-               console.log('user profile did not update')
-        }
-        else{
-        const profileUpdate = await usersModel.findOneAndUpdate(
-            {_id:userId},
-            {
-                     firstName,
-                     lastName,
-                     email,
-                     password,
-                     phone,
-                     address,
-                     image,
-                     gender,
-            },
-            {new:true}       
-        )
-        if(profileUpdate){
+        console.log({image,firstName,email})
+        // if(!asure){
+        //  const imageName = req.file.filename
+        //     const profileUpdate = await usersModel.findOneAndUpdate(
+        //         {_id:userId},
+        //         {
+        //              image:image,
+        //              firstName,
+        //              lastName,
+        //              email,
+        //              password,
+        //              phone,
+        //              address,
+        //              gender,
+        //         },
+        //         {new:true}
+        //     )
+        //     if(profileUpdate){
+        //        res.status(200).send('profile is succesfully updated')
+        //        } else
+        //        console.log('user profile did not update')
+        // }
+        // else{
+        // const profileUpdate = await usersModel.findOneAndUpdate(
+        //     {_id:userId},
+        //     {
+        //          firstName,
+        //              lastName,
+        //              email,
+        //              password,
+        //              phone,
+        //              address,
+        //              gender,
+        //     },
+        //     {new:true}       
+        // )
+        // if(profileUpdate){
            
-           res.status(200).send('profile is succesfully updated')
-           } else
-           console.log('user profile did not update')
-        }
+        //    res.status(200).send('profile is succesfully updated')
+        //    } else
+        //    console.log('user profile did not update')
+        // }
     } catch (error) {
         res.status(500).json({error:error})
         console.log(error.message)
