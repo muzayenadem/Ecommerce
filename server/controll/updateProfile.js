29db4253577f037889ebaaecfd5137c6968e927b
muzayenadem <muzynadem@gmail.com>
2024-07-02 19:28:51 +0300

    changing the file storage from multer to firebase

--- server/controll/updateProfile.js
+++ server/controll/updateProfile.js
@@ -9,7 +9,7 @@ const updateProfile = async(req,res)=>{
         // const splitedCategory = category && category.split(',')\
         // const imageName = req.file.filename
         const file = req.file
-        console.log({file})
+        console.log({file,body:req.body})
         const asure = req.file == undefined ? true : false
        
         if(!asure){
