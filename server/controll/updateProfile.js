f49b23350806e8ab719ab4da31423e6c9b3d9e4b
muzayenadem <muzynadem@gmail.com>
2024-07-02 21:54:53 +0300

    changing the file storage from multer to firebase

--- server/controll/updateProfile.js
+++ server/controll/updateProfile.js
@@ -2,15 +2,8 @@ const usersModel = require('../models/usersModel')
 
 const updateProfile = async(req,res)=>{
     try {
-
         const {image,firstName,lastName,email,password,phone,address,gender,userId} = req.body
-
-        // const splitedTags =  tags && tags.split(',')
-        // const splitedCategory = category && category.split(',')\
-        // const imageName = req.file.filename
-        console.log({image:image,firstName,email,password})
-       
-        
+        console.log({image:image,firstName,email,password}) 
             const profileUpdate = await usersModel.findOneAndUpdate(
                 {_id:userId},
                 {
@@ -25,10 +18,10 @@ const updateProfile = async(req,res)=>{
                 },
                 {new:true}
             )
-            if(profileUpdate){
-               res.status(200).send('profile is succesfully updated')
-               } else
-               console.log('user profile did not update')
+        if(profileUpdate){
+            res.status(200).send('profile is succesfully updated')
+        } else
+            console.log('user profile did not update')
     } catch (error) {
         res.status(500).json({error:error})
         console.log(error.message)
