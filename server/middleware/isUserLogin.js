8329457d14a6ca7496331ff42209597cf7bca7a2
muzayenadem <muzynadem@gmail.com>
2024-07-05 23:41:58 +0300

    fixing error

--- server/middleware/isUserLogin.js
+++ server/middleware/isUserLogin.js
@@ -1,23 +1,47 @@
 const jwt = require('jsonwebtoken')
 const auth = require('./auth')
 const usersModel = require('../models/usersModel')
+const { Verify } = require('crypto')
 
 const isUserLogin = async(req,res, next) =>{
     try {
         const token = req.cookies.user
-        const verify = jwt.verify(token,process.env.PASSWORD)
-        const users = await usersModel.find({})
-        for (i in users){
-            if(auth){
-                const profile = await usersModel.findOneAndUpdate(
-                    {_id:users[i]._id},
-                    {
-                       active:true
-                    },
-                    {new:true}
-                )
-            }
+
+        
+        if(!token){
+            let offLineProfile = await usersModel.findOneAndUpdate(
+                {_id:verify.userId},
+                {
+                    active:true
+                },
+                {new:true}
+            )
+            return console.log({offLineProfile})
         }
+
+        const verify = jwt.verify(token,process.env.PASSWORD)
+
+        if(!verify){
+            let offLineProfile = await usersModel.findOneAndUpdate(
+                {_id:verify.userId},
+                {
+                    active:true
+                },
+                {new:true}
+            )
+            return console.log({offLineProfile})
+        }
+
+        const onlineProfile = await usersModel.findOneAndUpdate(
+            {_id:verify.userId},
+            {
+                active:true
+            },
+            {new:true}
+        )
+        console.log({onlineProfile})
+
+     
     } catch (error) {
         res.status(500).send('something is error at users login middleware')
     }
